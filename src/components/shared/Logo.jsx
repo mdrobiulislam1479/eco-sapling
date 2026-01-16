2344456dddc2bc4accb73c306ec6b78d4458b8e2
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-01-16 11:59:11 +0600

    added navbar in root layout

---
+++ src/components/shared/Logo.jsx
@@ -0,0 +1,16 @@
+import { GiBonsaiTree } from "react-icons/gi";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center gap-2 text-3xl">
+      <div className="text-green-700">
+        <GiBonsaiTree />
+      </div>
+      <p className="font-bold">
+        Eco<span className="text-green-700">Sapling</span>
+      </p>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Logo from "./Logo";
+import NavLink from "../buttons/NavLink";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="bg-green-100">
+      <div className="container mx-auto py-8 flex flex-col sm:flex-row justify-between items-center gap-5">
+        <Logo />
+        <div className="flex gap-6 items-center">
+          <NavLink href={"/"}>Home</NavLink>
+          <NavLink href={"/plants"}>Plants</NavLink>
+          <Link
+            href={"/login"}
+            className="px-5 py-2 bg-green-700 rounded-md text-white font-medium"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
